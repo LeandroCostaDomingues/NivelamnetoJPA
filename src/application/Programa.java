0603de4f7e259b8517b6e285eb84d39edb0d5e47
leandro <leandrocosta190991@gmail.com>
2021-12-13 21:43:43 -0300

    Projeto Criado

---
+++ src/application/Programa.java
@@ -0,0 +1,16 @@
+package application;
+
+import dominio.Pessoas;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pessoas p1 = new Pessoas("João Pedro", "joaopedro@gmail.com", null);
+		Pessoas p2 = new Pessoas("maria jose", "maria@gmail.com", null);
+		System.out.println(p1);
+		System.out.println(p2);
+		
+	}
+
+}
